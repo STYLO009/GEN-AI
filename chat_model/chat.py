e688f415d39db7b3e30334c5864199cfdf082a3b
Abir Majumdar <abirmajumdar112@gmail.com>
2026-03-23 22:46:40 +0530

    Updated

--- chat_model/chat.py
+++ chat_model/chat.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
-model = init_chat_model("google_genai:gemini-2.5-flash-lite")
+from langchain_groq import ChatGroq
+model = ChatGroq(model = 'openai/gpt-oss-120b')
 
 invoke = input("Enter what you want to ask : ")
 response = model.invoke(invoke)
